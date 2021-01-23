5af46911ca1ce676ffb9b36062ad88196afd91e5
Nishant Tyagi <tyagi11n@gmail.com>
2021-01-23 11:20:43 +0530

    Reverse array OPTIMISED method

--- Reverse.java
+++ Reverse.java
@@ -1,15 +1,26 @@
 public class Reverse {
+    static void reverse_array(int arr[], int start, int end) {
 
-    static void reverse_array_recursive(int[] arr, int start, int end) {
-        if (start >= end) {
-            return;
+        int temp = 0;
+        while (start < end) {
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
-        int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-
-        reverse_array_recursive(arr, start + 1, end - 1);
     }
+
+    // static void reverse_array_recursive(int[] arr, int start, int end) {
+    // if (start >= end) {
+    // return;
+    // }
+    // int temp = arr[start];
+    // arr[start] = arr[end];
+    // arr[end] = temp;
+
+    // reverse_array_recursive(arr, start + 1, end - 1);
+    // }
 
     static void print_array(int[] arr, int n) {
         System.out.print("Reversed Array : ");
@@ -23,7 +34,8 @@ public class Reverse {
         int arr[] = { 1, 2, 3, 4, 5 };
         int size = arr.length;
 
-        reverse_array_recursive(arr, 0, size - 1);
+        reverse_array(arr, 0, size - 1);
+        // reverse_array_recursive(arr, 0, size - 1);
         print_array(arr, size);
     }
 }
